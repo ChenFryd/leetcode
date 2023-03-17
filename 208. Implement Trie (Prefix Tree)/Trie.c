82e35a2eb1ff66b6f398232bf82f550d296641cc
ChenFryd <chenfrydman@gmail.com>
2023-03-17 09:39:42 +0200

    Daily challange 17/03/2023
    created file

---
+++ 208. Implement Trie (Prefix Tree)/Trie.c
@@ -0,0 +1,42 @@
+typedef struct {
+    char val;
+    Node* right;
+    Node*; left;
+} Node* Node;
+
+typedef struct {
+    Node* root;
+} Trie;
+
+Trie* trieCreate() {
+    Trie* trieMain;
+    return trieMain;
+}
+
+void trieInsert(Trie* obj, char * word) {
+  
+}
+
+bool trieSearch(Trie* obj, char * word) {
+  
+}
+
+bool trieStartsWith(Trie* obj, char * prefix) {
+  
+}
+
+void trieFree(Trie* obj) {
+    
+}
+
+/**
+ * Your Trie struct will be instantiated and called as such:
+ * Trie* obj = trieCreate();
+ * trieInsert(obj, word);
+ 
+ * bool param_2 = trieSearch(obj, word);
+ 
+ * bool param_3 = trieStartsWith(obj, prefix);
+ 
+ * trieFree(obj);
+*/

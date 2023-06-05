7cd351302ed37a75eeec65ae58969addb6d1bd46
ChenFryd <chenfrydman@gmail.com>
2023-06-05 20:00:47 +0300

    05/06/2023

--- 67. Add Binary/addBinary.cpp
+++ 67. Add Binary/addBinary.cpp
@@ -10,15 +10,21 @@ public:
         std::string res = "";
 
         while (i >= 0 || j >= 0 || carry) {
-            carry += i >= 0 ? a[i--] - '0' : 0;
-            carry += j >= 0 ? b[j--] - '0' : 0;
+            carry += i >= 0 ? a[i] - '0' : 0;
+            carry += j >= 0 ? b[j] - '0' : 0;
+            i--; j--;
             res = char(carry % 2 + '0') + res;
             carry /= 2;
         }
 
+
+
+
+
         return res;
     }
 };
+
 
 int main() {
     Solution sol;

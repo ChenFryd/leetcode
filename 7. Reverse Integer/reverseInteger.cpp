70d20155b5b2be6ba684604929ab546459d30ea3
ChenFryd <chenfrydman@gmail.com>
2023-06-09 08:38:56 +0300

    07/09/2023

---
+++ 7. Reverse Integer/reverseInteger.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {                      
+public:
+    int reverse(int x) {
+        int r=0;      // decleare r 
+        while(x){
+            if (r>INT_MAX/10 || r<INT_MIN/10) return 0; // check 32 bit range if r is outside the range then return 0 
+            r=r*10+x%10; // find remainder and add its to r
+            x /= 10;     // Update the value of x
+        } 
+        return int(r);  // if r in the 32 bit range then return r
+    }
+}; 
+
+int main(){
+    Solution s;
+    int x = 123;
+    int ans = s.reverse(x);
+    cout<<ans<<endl;
+}

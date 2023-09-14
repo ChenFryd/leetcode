36e338ed56c8645e7721c0e54b442f999c02d20a
ChenFryd <chenfrydman@gmail.com>
2023-09-14 19:06:41 +0300

    14/09/2023

---
+++ 36. Valid Sudoku/validSudoku.cpp
@@ -0,0 +1,65 @@
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        // check row
+        for (int row = 0; row < 9; row++)
+        {
+            unordered_set<char> check_row;
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row][col] != '.' && check_row.find(board[row][col]) != check_row.end())
+                    return false;
+                check_row.insert(board[row][col]);
+            }
+        }
+
+        // check col
+        for (int col = 0; col < 9; col++)
+        {
+            unordered_set<char> check_col;
+            for (int row = 0; row < 9; row++)
+            {
+                if (board[row][col] != '.' && check_col.find(board[row][col]) != check_col.end())
+                    return false;
+                check_col.insert(board[row][col]);
+            }
+        }
+
+        // check cube
+        vector<unordered_set<char>> check_subbox(9);
+        for (int row = 0; row < 9; row++)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                int loc = (row / 3) * 3 + col / 3;
+                if (board[row][col] != '.' && check_subbox[loc].find(board[row][col]) != check_subbox[loc].end())
+                    return false;
+                check_subbox[loc].insert(board[row][col]);
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<char>> board = {{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                                  {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                                  {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                                  {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                                  {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                                  {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                                  {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                                  {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                                  {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+
+    cout << sol.isValidSudoku(board) << endl;
+}

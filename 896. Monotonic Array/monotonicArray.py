73ee6d455fcf412367e9828fec146090a8739b22
ChenFryd <chenfrydman@gmail.com>
2023-09-30 09:33:40 +0300

    Daily Challange 30/09/2023

---
+++ 896. Monotonic Array/monotonicArray.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def isMonotonic(self, A: List[int]) -> bool:
+        return all(A[i] <= A[i + 1] for i in range(len(A) - 1)) or all(A[i] >= A[i + 1] for i in range(len(A) - 1))
